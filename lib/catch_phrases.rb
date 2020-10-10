d68f51a97b4e3c8c02811adf3ec87495926c0878
Keith Williams <thestreetkw@hotmail.com>
2020-10-10 23:17:55 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,8 @@
 def mario
-  $status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   phrase = "It's-a me, Mario!"
   puts phrase
 end
-
-mario
 
 def toadstool
   puts status
